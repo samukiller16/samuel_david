c80a60c5e34f6384cfba76005a63f2b634b57df1
b14-23t <b14-23t@AULAB14-PC23.CICLOS.local>
2023-02-28 19:54:15 +0100

    Se ha creado la clase y se ha renombrado un atributo.

---
+++ ProyectoGit/src/circulo/Circle.java
@@ -0,0 +1,10 @@
+package circulo;
+
+public class Circle {
+	
+	   private double radio;
+
+	   public double getArea() {
+	      return Math.PI * radio * radio;
+	   }
+	}
---
+++ ProyectoGit/src/persona/Persona.java
@@ -0,0 +1,10 @@
+package persona;
+
+public class Persona {
+
+    private String nombre;
+
+    public void saludar() {
+        System.out.println("Hola, mi nombre es " + nombre);
+    }
+}
