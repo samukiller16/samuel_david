ca551ffa7b2152929a696f7a6659c8a109eb311d
b14-23t <b14-23t@AULAB14-PC23.CICLOS.local>
2023-02-28 20:33:47 +0100

    Hemos añadido otro atributo para el genero en la segunda clase.

--- ProyectoGit/src/persona/Persona.java
+++ ProyectoGit/src/persona/Persona.java
@@ -4,6 +4,7 @@ public class Persona {
 
     private String nombre;
     private int edad;
+    private String genero;
     
     public void saludar() {
         System.out.println("Hola, mi nombre es " + nombre);
