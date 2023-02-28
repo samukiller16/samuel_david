685d4d712ec5c292483d2931d89c2ee271847c5c
samue <samue@DESKTOP-COMJLJN.mshome.net>
2023-02-28 20:31:11 +0100

    Hemos añadido atributo edad

--- ProyectoGit/src/persona/Persona.java
+++ ProyectoGit/src/persona/Persona.java
@@ -3,7 +3,8 @@ package persona;
 public class Persona {
 
     private String nombre;
-
+    private int edad;
+    
     public void saludar() {
         System.out.println("Hola, mi nombre es " + nombre);
     }
